d530548c28cbf325d2dbd1f67483148479f7168b
faisalhamdi <46434308+faisalhamdi@users.noreply.github.com>
2020-01-15 14:00:48 +0700

    Revert "add feature schema db"

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,8 +11,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call(ProvinceTableSeeder::class);
-        $this->call(CityTableSeeder::class);
-        $this->call(DistrictTableSeeder::class);
+        // $this->call(UsersTableSeeder::class);
     }
 }
