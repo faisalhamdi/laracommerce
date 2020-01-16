4e9d6b8db78a6f9488bad611174339f04cb3502a
jambrongbro <jambrongbro17@gmail.com>
2020-01-16 11:27:39 +0700

    add feature templating and authentication

---
+++ resources/views/layouts/module/sidebar.blade.php
@@ -0,0 +1,28 @@
+<nav class="sidebar-nav">
+    <ul class="nav">
+        <li class="nav-item">
+            <a class="nav-link" href="#">
+                <i class="nav-icon icon-speedometer"></i> Dashboard
+            </a>
+        </li>
+
+        <li class="nav-title">PRODUCT MANAGEMENT</li>
+        <li class="nav-item">
+            <a class="nav-link" href="#">
+                <i class="nav-icon icon-drop"></i> Category
+            </a>
+        </li>
+        <li class="nav-item nav-dropdown">
+            <a class="nav-link nav-dropdown-toggle" href="#">
+                <i class="nav-icon icon-settings"></i> Setting
+            </a>
+            <ul class="nav-dropdown-items">
+                <li class="nav-item">
+                    <a class="nav-link" href="#">
+                        <i class="nav-icon icon-puzzle"></i> Shop
+                    </a>
+                </li>
+            </ul>
+        </li>
+    </ul>
+</nav>
