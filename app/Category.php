9a9f9f8b9f087d853d3c99117648747ccf6818d7
jambrongbro <jambrongbro17@gmail.com>
2020-01-17 09:48:45 +0700

    add feature management category

--- app/Category.php
+++ app/Category.php
@@ -3,8 +3,38 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Support\Str;
 
 class Category extends Model
 {
-    //
+    use SoftDeletes;
+
+    protected $fillable = [
+        'name',
+        'parent_id',
+        'slug'
+    ];
+
+    public function parent() {
+        return $this->belongsTo(Category::class);
+    }   
+
+    public function child() {
+        return $this->hasMany(Category::class, 'parent_id');
+    }
+
+    public function scopeGetParent($query) {
+        return $query->whereNull('parent_id');
+    }
+
+    // mutator
+    public function setSlugAttribute($value) {
+        $this->attributes['slug'] = Str::slug($value);
+    }
+
+    // accessor
+    public function getNameAttribute($value) {
+        return ucfirst($value);
+    }
 }
