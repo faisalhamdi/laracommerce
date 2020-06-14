7dd8d0fac11292afb42a99cebe4f92f9cb963002
faisalhamdi <faisal.hamdi@yahoo.com>
2020-06-14 21:54:31 +0700

    update logo login route

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -38,12 +38,17 @@
                                         <i class="icon-lock"></i>
                                     </span>
                                 </div>
-                                <input class="form-control {{ $errors->has('email') ? ' is-invalid' : '' }}" 
+                                <div>
+                                    <input class="form-control {{ $errors->has('email') ? ' is-invalid' : '' }}" 
                                     type="password" 
                                     name="password"
                                     placeholder="Password" 
+                                    id="password-field"
                                     required>
+                                    <span toggle="#password-field" class="fa fa-fw fa-eye field-icon toggle-password" style="float:right;margin-top:-22px;padding-right:24px;"></span>
+                                </div>
                             </div>
+                                
                             <div class="row">
                                 @if (session('error'))
                                 <div class="col-md-12">
